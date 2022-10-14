3ba21d4dc4913c051cdf1d9732825016d021a1cc
Artur <artur.gikramin@yandex.ru>
2022-10-15 02:03:13 +0500

    Variables

---
+++ 1_seminar/one_seminar.java
@@ -0,0 +1,29 @@
+
+public class one_seminar {
+    public static void main(String[] args) {
+       int a = 19; 
+       int b = 99;
+       int c = 3;
+       int d = 3;
+       int k = a;
+       if (b % c  == a ){
+        a = a * a (b % a);
+        System.out.println(a);
+       }
+       else
+       while (a < (b-d)) {
+         k = a;
+         a *= c;
+    }
+        a = k;
+        while  (a <= b ) { 
+         k = a;
+            a += d; 
+        }
+        if (k != b ){
+        System.out.println("Fuck");
+        }
+    System.out.println(k);
+}
+
+}
